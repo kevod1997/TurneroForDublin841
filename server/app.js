fb8e1bc2462cd89a54d055b2ed2ee45136a57793
Kevin Defalco <kevindefalco@gmail.com>
2023-08-31 00:33:17 -0300

    persist cross origin cookies

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ import cors from 'cors'
 
 const app = express()
 
-app.use(cors({credentials: true, origin: ["http://localhost:3000", "https://dublin841.shop"]}))
+app.use(cors({credentials: true, allowedHeaders: ['Content-Type', 'Authorization'], origin: ["http://localhost:3000", "https://dublin841.shop"]}))
 app.use(express.json())
 app.use(cookieParser())
 
