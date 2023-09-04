2d281b06cbf361d8a7fd416c0f57ae2a8db00c7b
Kevin Defalco <kevindefalco@gmail.com>
2023-09-04 16:53:47 -0300

    auth fix

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -3,6 +3,8 @@ import axios from "./axios";
 
 export const loginRequest = (admin) => axios.post('/login', admin);
 
-export const verifyTokenRequest = () => axios.get('/verify');
+export const verifyTokenRequest = () => axios.get('/verify', {
+    withCredentials: true, // Habilita el envío de cookies
+  });
 
 export const logoutRequest = () => axios.post('/logout');
